b33ed19acbeab9c0ec5a10954841b368bf123b9d
Kent Gruber <kgruber1@emich.edu>
2017-07-02 17:29:11 -0400

    Minor update to dataset line option. Stepped + tension

--- lib/chart_js/chart/dataset/line.rb
+++ lib/chart_js/chart/dataset/line.rb
@@ -10,7 +10,32 @@ module ChartJS
       instance_eval(&block)
       @container 
     end
+
+    def stepped(value = true)
+      @container['steppedLine'] = case value
+      when true # Step-before Interpolation -> eq "before"
+        true
+      when false # No Step Interpolation
+        false
+      when :before || "before"
+        "before"
+      when :after || "after"
+        "after"
+      else
+        raise "Oops."
+      end
+    end
     
+    def tension(value = 1)
+      if value.is_a? Integer
+        @container['lineTension'] = value
+      elsif value == "flase" ||  value == :false
+        @container['lineTension'] = 0
+      else
+        raise "Oops"
+      end
+    end
+
   end
 
 end
