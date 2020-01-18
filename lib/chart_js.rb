0a79f98746326d540bd163e980971c2c50029fd2
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-01-18 11:44:04 -0500

    Update chart_js.rb
    Remove pry

--- lib/chart_js.rb
+++ lib/chart_js.rb
@@ -1,36 +1,35 @@
 # coding: utf-8
-# This file is part of ChartJS 
+# This file is part of ChartJS
 # See https://github.com/picatz/chart_js for more information.
 # Copyright (C) 2017 Kent 'picat' Gruber
 # This program is published under MIT license.
 
-require 'pry'
 require "chart_js/version"
 require "chart_js/chart/chart"
 
-# Chart JS is a simple yet flexible JavaScript charting library. This 
-# gem is a Ruby Domain Specific Language which allows you to easily 
+# Chart JS is a simple yet flexible JavaScript charting library. This
+# gem is a Ruby Domain Specific Language which allows you to easily
 # build charts without touching a single line of JavaScript or HTML.
 # @author Kent 'picat' Gruber
 module ChartJS
   # NOTE: I did not write every word of this inline documentation and credit
   # much of it to to people over at http://www.chartjs.org/
- 
-  # Build a chart, do some stuff!  
+
+  # Build a chart, do some stuff!
   def self.build(&block)
     Chart.new(&block)
   end
 
-  # A line chart is a way of plotting data points on a line. Often, 
+  # A line chart is a way of plotting data points on a line. Often,
   # it is used to show trend data, or the comparison of two data sets.
   def self.line(&block)
     chart = Chart.new
     chart.type('line')
     chart.build(&block)
   end
-  
-  # A radar chart is a way of showing multiple data points and 
-  # the variation between them. They are often useful for 
+
+  # A radar chart is a way of showing multiple data points and
+  # the variation between them. They are often useful for
   # comparing the points of two or more different data sets.
   def self.radar(&block)
     chart = Chart.new
@@ -38,9 +37,9 @@ module ChartJS
     chart.build(&block)
   end
 
-  # Pie charts are probably one of the most commonly used charts. 
-  # They are divided into segments, the arc of each segment 
-  # shows the proportional value of each piece of data. They are 
+  # Pie charts are probably one of the most commonly used charts.
+  # They are divided into segments, the arc of each segment
+  # shows the proportional value of each piece of data. They are
   # excellent at showing the relational proportions between data.
   def self.pie(&block)
     chart = Chart.new
@@ -48,9 +47,9 @@ module ChartJS
     chart.build(&block)
   end
 
-  # Doughnut charts are probably one of the most commonly used charts. 
-  # They are divided into segments, the arc of each segment 
-  # shows the proportional value of each piece of data. They are 
+  # Doughnut charts are probably one of the most commonly used charts.
+  # They are divided into segments, the arc of each segment
+  # shows the proportional value of each piece of data. They are
   # excellent at showing the relational proportions between data but
   # with a big'O hole in the middle.
   def self.doughnut(&block)
@@ -59,8 +58,8 @@ module ChartJS
     chart.build(&block)
   end
 
-  # A bar chart provides a way of showing data values represented 
-  # as vertical bars. It is sometimes used to show trend data, 
+  # A bar chart provides a way of showing data values represented
+  # as vertical bars. It is sometimes used to show trend data,
   # and the comparison of multiple data sets side by side.
   def self.bar(type = :vertical, &block)
     chart = Chart.new
@@ -74,9 +73,9 @@ module ChartJS
   end
 
   # Need to send some server-side event data? You found your method. Optionally, it'll
-  # even call the "to_json" on the object your sending ( if it accepts 
-  # that method call ); otherwise you should really only send 
-  # JSON that is already formatted. Otherwise it'll just send the plaintext. 
+  # even call the "to_json" on the object your sending ( if it accepts
+  # that method call ); otherwise you should really only send
+  # JSON that is already formatted. Otherwise it'll just send the plaintext.
   #
   # Live life the way you want to.
   def self.data(json: false, &block)
